29eb7465571790c3cf7fb1fbad3b8b34a0f55313
Haeun Jeong <gkdms4248@gmail.com>
2023-06-06 23:44:36 +0900

    1.6 chat

--- zoom/src/public/js/app.js
+++ zoom/src/public/js/app.js
@@ -1,1 +1,26 @@
-alert("hi"); 
+// socket : 서버로의 연결
+const socket = new WebSocket(`ws://${window.location.host}`);
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
+
+socket.addEventListener("open", () => {
+    console.log("Connected to Server ✅");
+});
+
+socket.addEventListener("message", (message) => {
+    console.log("New message: ", message.data);
+});
+
+socket.addEventListener("close", () => {
+    console.log("Disconnected to Server ❌");
+});
+
+
+function handleSubmit() {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(input.value);
+    input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
--- zoom/src/server.js
+++ zoom/src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -5,8 +7,31 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
+// localhost:3000 은 http, ws 작동시킬 수 있음
 const handleListen = () => console.log('Listening on http://localhost:3000');
-app.listen(3000, handleListen);
+
+// 둘 다 돌리기 위한 작업
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server }); 
+
+// socket : 연결된 브라우저
+// function handleConnection(socket) {
+//     console.log(socket)
+// }
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+    sockets.push(socket);
+    console.log("Connected to Browser ✅");
+    socket.on("close", () => console.log("Disconnected to Browser ❌"));
+    socket.on("message", (message) => {
+        sockets.forEach(aSocket => aSocket.send(message.toString("utf-8")));
+        socket.send(message.toString("utf-8"));
+    });
+ });
+
+server.listen(3000, handleListen);
